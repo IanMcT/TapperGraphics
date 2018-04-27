7ffe9c96a852fe3067c5f35f47f7fe12f8f121a5
IanMcT <ian.mctavish@tldsb.on.ca>
2018-04-27 11:42:43 -0400

    Add files via upload

---
+++ tapperGraphics/tapperGraphics/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tapperGraphics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BitmapImage bitmap = new BitmapImage(new Uri("tapped.png", UriKind.Relative));
+            canvas.Background = new ImageBrush(bitmap);
+            drawSemiTransparentBackground();
+
+            drawServingBars();
+        }
+
+        private void drawSemiTransparentBackground()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Fill = new SolidColorBrush(Color.FromArgb(175, 255, 255, 255));
+            rectangle.Width = this.Width;
+            rectangle.Height = this.Height;
+            canvas.Children.Add(rectangle);
+        }
+
+        private void drawServingBars()
+        {
+            for (int i = 0; i < this.Height; i = i + 78)
+            {
+                Rectangle r = new Rectangle();
+                r.Fill = Brushes.Red;
+                r.Height = 2;
+                r.Width = this.Width;
+                canvas.Children.Add(r);
+                Canvas.SetTop(r, i);
+            }
+
+            Rectangle topBar = new Rectangle();
+            topBar.Width = 420;
+            topBar.Height = 55;
+            topBar.Fill = Brushes.Blue;
+            canvas.Children.Add(topBar);
+            Canvas.SetTop(topBar, 125);
+            Canvas.SetLeft(topBar, 200);
+
+            Rectangle secondBar = new Rectangle();
+            secondBar.Width = 540;
+            secondBar.Height = 55;
+            secondBar.Fill = Brushes.Yellow;
+            canvas.Children.Add(secondBar);
+            Canvas.SetTop(secondBar, 234);
+            Canvas.SetLeft(secondBar, 140);
+
+            Rectangle thirdBar = new Rectangle();
+            thirdBar.Width = secondBar.Width + (secondBar.Width - topBar.Width);
+            thirdBar.Height = 55;
+            thirdBar.Fill = Brushes.Pink;
+            canvas.Children.Add(thirdBar);
+            Canvas.SetTop(thirdBar, Canvas.GetTop(secondBar) + (Canvas.GetTop(secondBar) - Canvas.GetTop(topBar)));
+            Canvas.SetLeft(thirdBar, Canvas.GetLeft(secondBar) + (Canvas.GetLeft(secondBar) - Canvas.GetLeft(topBar)));
+
+            Rectangle bottomBar = new Rectangle();
+            bottomBar.Width = thirdBar.Width + (thirdBar.Width - secondBar.Width);
+            bottomBar.Height = 55;
+            bottomBar.Fill = Brushes.Green;
+            canvas.Children.Add(bottomBar);
+            Canvas.SetTop(bottomBar, Canvas.GetTop(thirdBar) + (Canvas.GetTop(thirdBar) - Canvas.GetTop(secondBar)));
+            Canvas.SetLeft(bottomBar, Canvas.GetLeft(thirdBar) + (Canvas.GetLeft(thirdBar) - Canvas.GetLeft(secondBar)));
+        }
+    }
+}
